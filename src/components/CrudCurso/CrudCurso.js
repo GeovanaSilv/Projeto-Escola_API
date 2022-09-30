fffbf2bfbc62f13b8d7e0583d2ae5c349b2aa27f
GEOVANA OLIVEIRA BARROS DA SILVA <u21430@LAPA17.academico.cotuca.unicamp.br>
2022-09-30 17:42:39 -0300

    Nova atualizacao sexta

--- src/components/CrudCurso/CrudCurso.js
+++ src/components/CrudCurso/CrudCurso.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react";
+
 import axios from 'axios';
 import './CrudCurso.css';
 import Main from '../template/Main';
+import { useState } from 'react';
 //import { resolveTo } from "react-router/lib/router";
 
 const title ="Cadastro de Curso";
@@ -13,84 +14,90 @@ const initialState={
     lista:[]
 }
 
-export default class CrudCurso extends Component{
-state = {...initialState}
-componentDidMount(){
-axios(urlAPI).then(resp =>{
-    this.setState({lista:resp.data})
-})
-}
-limpar(){
-    this.setState({aluno : initialState.aluno});
-}
-salvar(){
-    const  curso= this.state.curso;
-    curso.codCurso = Number(curso.codCurso);
-    const metodo = curso.id?'put':'post';
-    const url = curso.id ? `${urlAPI}/${curso.id}`:urlAPI;
+export default function CrudCurso(){
 
-    axios[metodo](url,curso)
-    .then(resp =>{
-        const lista = this.getListaAtualizada(resp.data)
-       this.setState({curso:initialState.curso,lista})
-    })
-}
-getListaAtualizada(curso, add = true){
-     const lista = this.state.lista.filter(a =>a.id !== curso.id);
-    if(add) lista.unshift(curso);
-    return lista;
-}
-atualizaCampo(evento){
-    const curso = {...this.state.curso};
-     curso[evento.target.name]= evento.target.value;
-   
-    this.setState({curso});
-}
-carregar(curso){
-    this.setState({curso})
-    const url = urlAPI + "/" + curso.id;
-    if(window.confirm("Confirma alteração "+curso.id));
-    axios['PUT'](url,curso)
-    .then(resp =>{
-        const lista = this.getListaAtualizada(curso,false)
-        this.setState({curso:initialState.curso,lista})
-    })
-}
-remover(curso){
-    const url = urlAPI + "/"+curso.id;
-    if(window.confirm("Confirma remoção do curso:"+curso.codCurso)){
+const[setCurso] = useState();
+
+   function limpar(){
+        setCurso({curso : initialState.curso});
+       }
+
+      function salvar(){
+        const curso = this.setCurso.curso;
+        curso.codCurso = Number(curso.codCurso);
+        const metodo = curso.id? 'put':'post';
+        const url = curso.id ? `${urlAPI}/${curso.id}`: urlAPI;
+     
+        axios[metodo](url,curso)
+        .then(resp =>{
+            const lista = this.getListaAtualizada(resp.data)
+          setCurso({curso:initialState.curso,lista})
+        })     
+    }
+ function   getListaAtualizada(curso, add = true){
+        const lista = this.setCurso.lista.filter(a => a.id !== curso.id);
+        if(add) lista.unshift(curso);
+        return lista;
+       }
+
+
+    function   atualizaCampo(evento){
+        const curso  = {...this.setCurso.curso};
+    
+        curso[evento.target.name]= evento.target.value;
+    
+        this.setCurso({curso});
+       }
+
+
+     function  carregar(curso) {
+        this.setCurso({curso})
+           const url = urlAPI + "/" + curso.id;
+           if (window.confirm("Confirma alteração curso: " + curso.codCurso)) {
+           console.log("entrou no confirm");
+           axios['PUT'](url, curso)
+           .then(resp => {
+           const lista = this.getListaAtualizada(curso, false)
+           this.setCurso({ curso: initialState.curso, lista })
+       })
+    
+    }
+    this.atualizaCampo(curso);
+    }
+ function remover(curso){
+    const url = urlAPI + "/"+ curso.id;
+    if(window.confirm("Confirma remoção do curso:" + curso.nomeCurso)){
         console.log("entrou no confirm");
 
         axios['delete'](url,curso)
-        .then(resp=>{
+        .then(resp =>{
             const lista = this.getListaAtualizada(curso,false)
-            this.setState({curso:initialState.curso,lista})
+            this.setCurso({curso:initialState.curso,lista})
         })
     }
-}
-renderForm(){
+  }
+   const renderForm = () =>{
     return(
-       <div  className="inclui-container">
-        
+        <div  className="inclui-container">
+          <label> Nome: </label>
+          <input
+           type ="text"
+           id="nome"
+            placeholder ="Nome do Curso"
+            className ="form-input"
+            name = "nome"
+            value={setCurso.curso.nomeCurso}
+            onChange={e =>this.atualizaCampo(e)}              
+            />
 
-           <label>Nome:</label>
-           <input
-            type="text"
-            id = "nomeCurso"
-            placeholder="Nome do curso"
-            className="form-input"
-            name="nome" 
-            value={this.state.curso.nomeCurso}
-            onChange={e => this.atualizaCampo(e)}
-           />
-             <label>Periodo:</label>
+           <label>Periodo:</label>
            <input
             type="text"
             id = "periodo"
-            placeholder="Periodo co Curso"
+            placeholder="Periodo do curso"
             className="form-input"
-            name="nome" 
-            value={this.state.curso.periodo}
+            name="periodo" 
+            value={setCurso.curso.periodo}
             onChange={e => this.atualizaCampo(e)}
            />
            <label>Código do Curso</label>
@@ -100,7 +107,7 @@ renderForm(){
              placeholder="0"
              className="form-input"
              name="codCurso"
-             value={this.state.curso.codCurso}
+             value={setCurso.curso.codCurso}
              onChange={e => this.atualizaCampo(e)}
              />
             <button className="btnSalvar"
@@ -114,22 +121,22 @@ renderForm(){
              </div>
     )
 }
-renderTable(){
+const renderTable =()  =>{
     return(
 <div className="listagem">
-   <table className="listaCursos"id ="tblListaCursos">
+   <table className="listaCurso"id ="tblListaCursos">
        <thead>
            <tr className="cabecTabela">
       <th className="tabTituloNome">Nome</th>
-      <th className="tabTituloCurso">periodo</th>    
-      <th className="tabTituloCurso">codCurso</th>    
+      <th className="tabTituloPeriodo">periodo</th>
+      <th className="tabTituloCurso">Codigo Curso</th>                  
            </tr>
        </thead>
        <tbody>
-         {this.state.lista.map(
+         {this.setCurso.lista.map(
              (curso) =>
              <tr key={curso.id}>
-                 <td>{curso.nomeCurso}</td>
+                 <td>{curso.nome}</td>
                  <td>{curso.periodo}</td>
                  <td>{curso.codCurso}</td>
                   <td>
@@ -150,13 +157,12 @@ renderTable(){
     </div>
     )
 }
-render(){
+} 
+  const render  = ()=>{
     return(
     <Main title={title}>
         {this.renderForm()}
        {this.renderTable()}
         </Main>
     )
-}
-
 }
